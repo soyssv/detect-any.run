a69704a5d8aea4ef8cf319a2a7cb92ff4cadac49
soyssv <243172113+soyssv@users.noreply.github.com>
2025-11-10 20:54:53 +0100

    Add files via upload

---
+++ detect.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal
+
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    PowerShell -Command "Start-Process cmd -ArgumentList '/c \"%~f0\"' -Verb RunAs"
+    exit /b
+)
+
+set "targetDir=C:\Program Files\KernelLogger"
+set "targetFile=%targetDir%\test.txt"
+set "success=0"
+
+if not exist "%targetDir%" (
+    mkdir "%targetDir%"
+    if %errorLevel% neq 0 (
+        goto :cleanup
+    )
+)
+
+echo https://github.com/soyssv > "%targetFile%"
+
+if %errorLevel% neq 0 (
+    goto :cleanup
+)
+
+set "success=1"
+type "%targetFile%"
+echo.
+
+:cleanup
+if exist "%targetFile%" (
+    del "%targetFile%"
+)
+
+if "%success%"=="1" (
+    echo any.run not detected
+) else (
+    echo any.run detected
+)
+
+echo.
+pause
